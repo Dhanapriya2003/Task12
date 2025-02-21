51198a2f0d0e6d53d5c84c4446e5bc557154f51d
Dhanapriya2003 <dhanapriya1662003@gmail.com>
2025-02-21 23:36:56 +0530

    cred changes

--- js/config.js
+++ js/config.js
@@ -1,1 +1,2 @@
-const API_BASE_URL = "http://localhost:8081/api/employees";
+// const API_BASE_URL = "http://localhost:8081/api/employees";
+const API_BASE_URL = "https://employee-management-app-cg30.onrender.com/api/employees";
